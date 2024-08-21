0488862c263fd7db754a66b33afb7e8358e594be
fatekkl <mathtml.1105@gmail.com>
2024-08-21 12:54:18 -0300

    finish activity

--- src/Exercicios/Logica/HoraDeCodar4.kt
+++ src/Exercicios/Logica/HoraDeCodar4.kt
@@ -1,7 +1,8 @@
 package exercicios.logica
 
-fun main() {
+import java.util.*
 
+fun main() {
     fun primeiro(list: MutableList<String> = mutableListOf()) {
 
         println("Qual o nome do/da estudante? caso queira parar digite PARE")
@@ -18,5 +19,43 @@ fun main() {
 
     }
 
-    primeiro()
+    fun segundo() {
+        val planets = arrayOf("TERRA", "MARTE", "PLUTÃO", "VÊNUS", "JUPITER", "SATURNO")
+
+        println("Insira o nome de um planeta: ")
+        val read = readln().uppercase(Locale.getDefault())
+
+        for (element in planets) {
+            if (element == read) {
+                println("O planeta $read esta na lista")
+                break
+            }
+            println("$read não esta na lista")
+            break
+        }
+    }
+
+    fun terceiro() {
+        val fruits = mutableListOf("Banana", "Manga", "Melancia")
+
+        for (element in fruits) {
+            println(element)
+        }
+
+        while (fruits.isNotEmpty()) {
+            println("Digite uma fruta para ser retirada: ")
+            val toDelete = readln()
+
+            if (fruits.remove(toDelete)) {
+                println("$toDelete foi removido")
+            } else {
+                println("$toDelete não foi encontrado!")
+
+            }
+
+        }
+
+        println("Lista de compras finalizada!")
+    }
+
 }

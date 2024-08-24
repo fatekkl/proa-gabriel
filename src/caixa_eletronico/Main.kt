7bd0d5739dad57e3c08c320d6bb24e162045a9dc
fatekkl <mathtml.1105@gmail.com>
2024-08-24 12:03:02 -0300

    start new activity

---
+++ src/caixa_eletronico/Main.kt
@@ -0,0 +1,12 @@
+package caixa_eletronico
+
+fun main() {
+
+    fun start() {
+        println("Ola, informe seu nome: ")
+        val nome = readln()
+        println("Ola $nome è um prazer ter você por aqui!")
+
+    }
+
+}

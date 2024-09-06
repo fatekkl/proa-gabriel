fa84774d0b9fd33c56337179049e00621919df7a
fatekkl <mathtml.1105@gmail.com>
2024-09-06 12:49:51 -0300

    implement confirm function call

--- src/hotel/src/models/Orange.kt
+++ src/hotel/src/models/Orange.kt
@@ -1,17 +1,18 @@
 package hotel.src.models
 
-class Orange : Auditorium(150, 70) {
+class Orange() : Auditorium(150, 70, "Laranja") {
 
     override fun reserve(visitants: Int) {
-
         if (visitants > chairs) {
             println("O Auditorio Laranja pode ser reservado, você vai precisar de ${visitants - chairs} cadeiras adicionais \n")
         } else {
             println("O Auditorio Laranja pode ser reservado \n")
+            super.event.guests = visitants
         }
         schedule()
         waiters(visitants)
         buffet(visitants)
+        confirm()
     }
 
 
--- src/hotel/src/models/Purple.kt
+++ src/hotel/src/models/Purple.kt
@@ -1,12 +1,13 @@
 package hotel.src.models
 
-class Purple: Auditorium(350, 0) {
+class Purple() : Auditorium(350,  0, "Colorado") {
 
     override fun reserve(visitants: Int) {
         println("O auditorio colorado foi reservado, nesse auditorio não são necessarias cadeiras adicionais")
         schedule()
         waiters(visitants)
         buffet(visitants)
+        confirm()
     }
 
 

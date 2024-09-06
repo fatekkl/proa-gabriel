93f7b56e9547d16bde6e277fd26af2ef546cd22c
fatekkl <mathtml.1105@gmail.com>
2024-09-06 10:26:51 -0300

    implement waiters method in Auditorium super class

--- src/hotel/src/models/Auditorium.kt
+++ src/hotel/src/models/Auditorium.kt
@@ -1,5 +1,7 @@
 package hotel.src.models
 
+import kotlin.math.ceil
+
 abstract class Auditorium(
     val chairs: Int,
     val additional: Int,
@@ -11,9 +13,47 @@ abstract class Auditorium(
         "sexta",
         "sabado",
         "domingo"
-    )
+    ),
 ) {
 
 
     abstract fun reserve(visitants: Int)
+
+    protected fun schedule() {
+        println("Agora vamos ver a agenda do evento \n")
+
+        println("Qual o dia do seu evento?")
+
+        val response = readln().lowercase()
+
+        if (week.indexOf(response) <= 3){
+            println("Qual a hora do seu evento?")
+            val hour = readln().toInt()
+
+            if (hour in 7..23) {
+                println("Qual o nome da sua empresa?")
+                val businessName = readln()
+
+                println("Auditorio reservado para a empresa $businessName. $response as ${hour}hs")
+            } else {
+                println("Auditorio indisponivel")
+            }
+        }
+    }
+
+    protected fun waiters(visitants: Int) {
+        val waiters = ceil( (visitants / 12).toDouble() ).toInt()
+        println("Serão necessarios $waiters garcons \n")
+
+        println("Qual a duracão do evento em horas? \n")
+        val hours = readln().toInt()
+
+        val waitersPrice = waiters * hours * 10.50
+
+        println("O Custo dos garcons sera $waitersPrice")
+    }
+
+    protected fun buffet() {
+
+    }
 }
--- src/hotel/src/models/Orange.kt
+++ src/hotel/src/models/Orange.kt
@@ -1,34 +1,17 @@
 package hotel.src.models
 
-class Orange: Auditorium(150, 70) {
+class Orange : Auditorium(150, 70) {
 
     override fun reserve(visitants: Int) {
 
         if (visitants > chairs) {
-            println("O Auditorio Laranja pode ser reservado, você vai precisar de ${visitants - chairs} cadeiras adicionais")
+            println("O Auditorio Laranja pode ser reservado, você vai precisar de ${visitants - chairs} cadeiras adicionais \n")
         } else {
             println("O Auditorio Laranja pode ser reservado \n")
         }
-
-        println("Agora vamos ver a agenda do evento \n")
-
-        println("Qual o dia do seu evento?")
-
-        val response = readln().lowercase()
-
-        if (super.week.indexOf(response) <= 3){
-            println("Qual a hora do seu evento?")
-            val hour = readln().toInt()
-
-            if (hour in 7..23) {
-                println("Qual o nome da sua empresa?")
-                val businessName = readln()
-
-                println("Auditorio reservado para a empresa $businessName. Dia$response as ${hour}hs")
-            } else {
-                println("Auditorio indisponivel")
-            }
-        }
-
+        schedule()
+        waiters(visitants)
     }
+
+
 }
